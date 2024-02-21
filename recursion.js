c078a673eaa696ee4afc69e12931101e4137848c
John Flores <johnflores1991@hotmail.com>
2024-02-21 13:42:42 -0600

    Added max recursive function.

--- recursion.js
+++ recursion.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // Sum nums in array
-function sum(list) {
-  if (list.length) {
+function sum(nums) {
+  if (nums.length) {
     return 0;
   }
 
@@ -16,4 +16,15 @@ function count(arr) {
   }
 
   return 1 + count(arr.slice(1));
+}
+
+// Find max num in array
+function max(list) {
+  if (list.length === 2) {
+    return list[0] > list[1] ? list[0] : list[1];
+  }
+
+  const sub_max = max(list.slice(1));
+  
+  return list[0] > sub_max ? list[0] : sub_max;
 }
